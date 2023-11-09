bdb7399a8168db50a87d270efea896bca8c2b1ed
TlexSHF <54903179+TlexSHF@users.noreply.github.com>
2023-11-09 15:01:37 +0100

    car goes probe, turns right or left if spotted an object, and stops when driving too close to an object. BUG: Sometimes the car does not rotate even when displaying R or L, & sometimes the car does not drive even when displaying P

--- Exploring/obj/b__main.ads
+++ Exploring/obj/b__main.ads
@@ -27,6 +27,8 @@ package ada_main is
 
    --  BEGIN ELABORATION ORDER
    --  ada%s
+   --  ada.characters%s
+   --  ada.characters.latin_1%s
    --  interfaces%s
    --  system%s
    --  ada.io_exceptions%s
@@ -35,6 +37,8 @@ package ada_main is
    --  gnat%s
    --  interfaces.nrf52%s
    --  interfaces.nrf52.clock%s
+   --  system.address_operations%s
+   --  system.address_operations%b
    --  system.bb%s
    --  system.bb.board_parameters%s
    --  system.bb.cpu_specific%s
@@ -78,6 +82,8 @@ package ada_main is
    --  system.wch_cnv%b
    --  system.address_image%s
    --  system.address_image%b
+   --  system.compare_array_unsigned_8%s
+   --  system.compare_array_unsigned_8%b
    --  system.traceback%s
    --  system.traceback%b
    --  ada.strings.utf_encoding%s
@@ -173,8 +179,20 @@ package ada_main is
    --  ada.real_time%b
    --  ada.real_time.delays%s
    --  ada.real_time.delays%b
+   --  ada.real_time.timing_events%s
+   --  ada.real_time.timing_events%b
    --  system.assertions%s
    --  system.assertions%b
+   --  system.bit_ops%s
+   --  system.bit_ops%b
+   --  ada.strings.maps%s
+   --  ada.strings.maps%b
+   --  ada.strings.search%s
+   --  ada.strings.search%b
+   --  ada.strings.fixed%s
+   --  ada.strings.fixed%b
+   --  ada.strings.unbounded%s
+   --  ada.strings.unbounded%b
    --  system.pool_global%s
    --  system.pool_global%b
    --  system.random_seed%s
@@ -221,6 +239,8 @@ package ada_main is
    --  nrf_svd.uart%s
    --  nrf_svd.uicr%s
    --  nrf_svd.wdt%s
+   --  generic_timers%s
+   --  generic_timers%b
    --  hal.gpio%s
    --  hal.i2c%s
    --  hal.spi%s
@@ -263,14 +283,21 @@ package ada_main is
    --  microbit.console%b
    --  dfr0548%s
    --  dfr0548%b
+   --  microbit.displayrt%s
+   --  microbit.displayrt%b
    --  microbit.i2c%s
    --  microbit.i2c%b
    --  microbit.iosfortasking%s
    --  microbit.iosfortasking%b
    --  microbit.motordriver%s
    --  microbit.motordriver%b
+   --  microbit.timehighspeed%s
+   --  microbit.timehighspeed%b
    --  microbit.timewithrtc1%s
    --  microbit.timewithrtc1%b
+   --  microbit.types%s
+   --  microbit.ultrasonic%s
+   --  microbit.ultrasonic%b
    --  tasks%s
    --  tasks%b
    --  main%b
--- Exploring/src/tasks.adb
+++ Exploring/src/tasks.adb
@@ -5,9 +5,14 @@ with MicroBit.TimeWithRTC1; use MicroBit.TimeWithRTC1;
 with MicroBit.IOsForTasking; use MicroBit.IOsForTasking;
 with MicroBit.Console;
 with Ada.Numerics.Discrete_Random;
+with MicroBit.DisplayRT;
 
 package body tasks is
 
+   -- Bytter aldri tilbake til probe state
+
+
+   -- Act
    task body Fare is
       -- Random Number Generator
       package Rand_Int is new ada.numerics.discrete_random(Angle);
@@ -20,32 +25,112 @@ package body tasks is
 
    begin
       Set_Analog_Period_Us(20000);
+      driveStart := Clock;
       loop
-         -- Car drives for "driveDuration"
-         driveStart := Clock;
-         MotorDriver.Drive(MotorDriver.Forward);
+         if probeState = Stop then
+            MotorDriver.Drive(MotorDriver.Stop);
+            driveStart := Clock;
+         else
 
-         delay until driveStart + driveDuration;
+            -- Car drives for "driveDuration"
+            MotorDriver.Drive(MotorDriver.Forward);
 
-         -- Car rotates for "wantedAngle" degrees
-         Rand_Int.reset(gen);
-         wantedAngle := Rand_Int.random(gen);
-         Rotate(wantedAngle);
+            -- delay until driveStart + driveDuration;
 
+            if probeState = probe and Clock >= driveStart + driveDuration then
+               -- Car rotates for "wantedAngle" degrees
+               Rand_Int.reset(gen);
+               wantedAngle := Rand_Int.random(gen);
+               Rotate(wantedAngle);
+               driveStart := Clock;
+
+            elsif probeState = GoToRight and  previousProbeState /= GoToRight then
+               previousProbeState := GoToRight;
+               Rotate(90, true);
+               driveStart := Clock;
+
+            elsif probeState = GoToLeft and previousProbeState /= GoToLeft then
+               previousProbeState := GoToLeft;
+               Rotate(90, false);
+               driveStart := Clock;
+            end if;
+         end if;
       end loop;
    end Fare;
 
+   -- Think
+   task body ProbeThink is
+      clockStart : Time;
+      period : Time_Span := Milliseconds(10);
+   begin
+      loop
+         clockStart := Clock;
 
-   procedure Rotate (wantedAngle : Angle) is
+         DisplayRT.Clear;
+         case probeState is
+            when Probe => DisplayRT.Display('P');
+            when GoToFront => DisplayRT.Display('F');
+            when GoToLeft => DisplayRT.Display('L');
+            when GoToRight => DisplayRT.Display('R');
+            when Stop => DisplayRT.Display('S');
+            when others => DisplayRT.Display('X'); exit;
+         end case;
+
+         if pollFlag then
+
+            if distanceFront > 0 and distanceFront <= 20 then
+               probeState := Stop;
+            elsif distanceFront > 0 and distanceFront <= 30 then
+               probeState := GoToFront;
+             elsif distanceRight > 0 and distanceRight <= 50 then
+               probeState := GoToRight;
+             elsif distanceLeft > 0 and distanceLeft <= 50 then
+               probeState := GoToLeft;
+            else
+               probeState := Probe;
+            end if;
+         end if;
+
+        delay until clockStart + period;
+      end loop;
+   end ProbeThink;
+
+   -- Sense (stolen shamelessly from Bendik)
+   task body PollEcho is
+      clockStart : Time;
+      period : Time_Span := Milliseconds(27);
+   begin
+      loop
+         clockStart := Clock;
+
+         distanceFront  := sensorFront.Read;
+         distanceRight  := sensorRight.Read;
+         distanceLeft   := sensorLeft.Read;
+
+         pollFlag := True;
+
+         if distanceFront = 0 then distanceFront := 400; end if;
+         if distanceRight = 0 then distanceRight := 400; end if;
+         if distanceLeft = 0 then distanceLeft := 400; end if;
+
+         delay until clockStart + period;
+      end loop;
+   end PollEcho;
+
+   procedure Rotate (wantedAngle : Angle; clockwise : Boolean := True) is
       angleDurationMicro : constant Integer := 9200;
       totalAngleDuration : Time_Span := Microseconds(Integer(wantedAngle) * angleDurationMicro);
       rotateStart : Time;
 
    begin
       rotateStart := Clock;
-      MotorDriver.Drive(MotorDriver.Rotating_Right);
+
+      if clockwise then
+         MotorDriver.Drive(MotorDriver.Rotating_Right);
+      else
+         MotorDriver.Drive(MotorDriver.Rotating_Left);
+      end if;
 
       delay until rotateStart + totalAngleDuration;
    end Rotate;
-
 end tasks;
