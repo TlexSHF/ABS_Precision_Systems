02b6f25509a9506e7e43cd0931c633581114c491
Bendik <benbea97@gmail.com>
2023-11-13 14:01:25 +0100

    changes to tasks.adb

--- motor_driver_ravenscar/src/main.adb
+++ motor_driver_ravenscar/src/main.adb
@@ -1,15 +1,15 @@
 with MicroBit; use MicroBit;
 --  with MicroBit.MotorDriver;
---  with MicroBit.Ultrasonic;
---  with MicroBit.Console; use MicroBit.Console;
+with MicroBit.Ultrasonic;
+with MicroBit.Console; use MicroBit.Console;
 --  with MicroBit.IOsForTasking; use MicroBit.IOsForTasking;
-with tasks; use tasks;
+--  with tasks; use tasks;
 
 procedure Main with Priority => 0 is
       --  lineTrackerMiddle : Boolean    := False;
       --  lineTrackerLeft  : Boolean    := False;
       --  lineTrackerRight : Boolean    := False;
-      --  package sensor is new MicroBit.Ultrasonic(MB_P12,MB_P0);
+      package sensor is new MicroBit.Ultrasonic(MB_P12,MB_P0);
 begin
    --  Set_Analog_Period_Us(20000);
    --  MotorDriver.Drive(MotorDriver.Forward, (1024, 0, 0, 0));
@@ -20,8 +20,8 @@ begin
       --  lineTrackerRight  := digitalRead (16);
       --  --
       --  Put_Line(lineTrackerLeft'Image & ", " & lineTrackerMiddle'Image & ", " & lineTrackerRight'Image);
-      --  Put_Line(sensor.Read'Image);
-      --  delay(0.5);
+      Put_Line(sensor.Read'Image);
+      delay(0.1);
       null;
    end loop;
 
